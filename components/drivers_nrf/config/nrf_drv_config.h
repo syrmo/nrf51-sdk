319fd2a44969f6cea4abc6317125e32d29a10ffa
Ponti Gaston <gaston.ponti@wolox.com.ar>
2016-01-14 14:43:53 -0300

    Defines in nrf_drv_config can be override

--- components/drivers_nrf/config/nrf_drv_config.h
+++ components/drivers_nrf/config/nrf_drv_config.h
@@ -20,7 +20,9 @@
 #define CLOCK_CONFIG_IRQ_PRIORITY       APP_IRQ_PRIORITY_LOW
 
 /* GPIOTE */
-#define GPIOTE_ENABLED 0
+#ifndef GPIOTE_ENABLED
+  #define GPIOTE_ENABLED 0
+#endif
 
 #if (GPIOTE_ENABLED == 1)
 #define GPIOTE_CONFIG_USE_SWI_EGU false
@@ -29,7 +31,9 @@
 #endif
 
 /* TIMER */
-#define TIMER0_ENABLED 0
+#ifndef TIMER0_ENABLED
+  #define TIMER0_ENABLED 0
+#endif
 
 #if (TIMER0_ENABLED == 1)
 #define TIMER0_CONFIG_FREQUENCY    NRF_TIMER_FREQ_16MHz
@@ -40,7 +44,9 @@
 #define TIMER0_INSTANCE_INDEX      0
 #endif
 
-#define TIMER1_ENABLED 0
+#ifndef TIMER1_ENABLED
+  #define TIMER1_ENABLED 0
+#endif
 
 #if (TIMER1_ENABLED == 1)
 #define TIMER1_CONFIG_FREQUENCY    NRF_TIMER_FREQ_16MHz
@@ -50,8 +56,10 @@
 
 #define TIMER1_INSTANCE_INDEX      (TIMER0_ENABLED)
 #endif
- 
-#define TIMER2_ENABLED 0
+
+#ifndef TIMER2_ENABLED
+  #define TIMER2_ENABLED 0
+#endif
 
 #if (TIMER2_ENABLED == 1)
 #define TIMER2_CONFIG_FREQUENCY    NRF_TIMER_FREQ_16MHz
@@ -62,7 +70,9 @@
 #define TIMER2_INSTANCE_INDEX      (TIMER1_ENABLED+TIMER0_ENABLED)
 #endif
 
-#define TIMER3_ENABLED 0
+#ifndef TIMER3_ENABLED
+  #define TIMER3_ENABLED 0
+#endif
 
 #if (TIMER3_ENABLED == 1)
 #define TIMER3_CONFIG_FREQUENCY    NRF_TIMER_FREQ_16MHz
@@ -73,7 +83,9 @@
 #define TIMER3_INSTANCE_INDEX      (TIMER2_ENABLED+TIMER1_ENABLED+TIMER0_ENABLED)
 #endif
 
-#define TIMER4_ENABLED 0
+#ifndef TIMER4_ENABLED
+  #define TIMER4_ENABLED 0
+#endif
 
 #if (TIMER4_ENABLED == 1)
 #define TIMER4_CONFIG_FREQUENCY    NRF_TIMER_FREQ_16MHz
@@ -88,7 +100,9 @@
 #define TIMER_COUNT (TIMER0_ENABLED + TIMER1_ENABLED + TIMER2_ENABLED + TIMER3_ENABLED + TIMER4_ENABLED)
 
 /* RTC */
-#define RTC0_ENABLED 0
+#ifndef RTC0_ENABLED
+  #define RTC0_ENABLED 0
+#endif
 
 #if (RTC0_ENABLED == 1)
 #define RTC0_CONFIG_FREQUENCY    32678
@@ -98,7 +112,9 @@
 #define RTC0_INSTANCE_INDEX      0
 #endif
 
-#define RTC1_ENABLED 0
+#ifndef RTC1_ENABLED
+  #define RTC1_ENABLED 0
+#endif
 
 #if (RTC1_ENABLED == 1)
 #define RTC1_CONFIG_FREQUENCY    32768
@@ -113,7 +129,9 @@
 #define NRF_MAXIMUM_LATENCY_US 2000
 
 /* RNG */
-#define RNG_ENABLED 0
+#ifndef RNG_ENABLED
+  #define RNG_ENABLED 0
+#endif
 
 #if (RNG_ENABLED == 1)
 #define RNG_CONFIG_ERROR_CORRECTION true
@@ -122,7 +140,9 @@
 #endif
 
 /* SPI */
-#define SPI0_ENABLED 0
+#ifndef SPI0_ENABLED
+  #define SPI0_ENABLED 0
+#endif
 
 #if (SPI0_ENABLED == 1)
 #define SPI0_USE_EASY_DMA 0
@@ -135,7 +155,9 @@
 #define SPI0_INSTANCE_INDEX 0
 #endif
 
-#define SPI1_ENABLED 0
+#ifndef SPI1_ENABLED
+  #define SPI1_ENABLED 0
+#endif
 
 #if (SPI1_ENABLED == 1)
 #define SPI1_USE_EASY_DMA 0
@@ -148,7 +170,9 @@
 #define SPI1_INSTANCE_INDEX (SPI0_ENABLED)
 #endif
 
-#define SPI2_ENABLED 0
+#ifndef SPI2_ENABLED
+  #define SPI2_ENABLED 0
+#endif
 
 #if (SPI2_ENABLED == 1)
 #define SPI2_USE_EASY_DMA 0
@@ -164,7 +188,9 @@
 #define SPI_COUNT   (SPI0_ENABLED + SPI1_ENABLED + SPI2_ENABLED)
 
 /* SPIS */
-#define SPIS0_ENABLED 0
+#ifndef SPIS0_ENABLED
+  #define SPIS0_ENABLED 0
+#endif
 
 #if (SPIS0_ENABLED == 1)
 #define SPIS0_CONFIG_SCK_PIN         2
@@ -175,7 +201,9 @@
 #define SPIS0_INSTANCE_INDEX 0
 #endif
 
-#define SPIS1_ENABLED 0
+#ifndef SPIS1_ENABLED
+  #define SPIS1_ENABLED 0
+#endif
 
 #if (SPIS1_ENABLED == 1)
 #define SPIS1_CONFIG_SCK_PIN         2
@@ -186,7 +214,9 @@
 #define SPIS1_INSTANCE_INDEX SPIS0_ENABLED
 #endif
 
-#define SPIS2_ENABLED 0
+#ifndef SPIS2_ENABLED
+  #define SPIS2_ENABLED 0
+#endif
 
 #if (SPIS2_ENABLED == 1)
 #define SPIS2_CONFIG_SCK_PIN         2
@@ -200,7 +230,9 @@
 #define SPIS_COUNT   (SPIS0_ENABLED + SPIS1_ENABLED + SPIS2_ENABLED)
 
 /* UART */
-#define UART0_ENABLED 0
+#ifndef UART0_ENABLED
+  #define UART0_ENABLED 0
+#endif
 
 #if (UART0_ENABLED == 1)
 #define UART0_CONFIG_HWFC         NRF_UART_HWFC_DISABLED
@@ -219,7 +251,9 @@
 #endif //NRF52
 #endif
 
-#define TWI0_ENABLED 0
+#ifndef TWI0_ENABLED
+  #define TWI0_ENABLED 0
+#endif
 
 #if (TWI0_ENABLED == 1)
 #define TWI0_CONFIG_FREQUENCY    NRF_TWI_FREQ_100K
@@ -230,7 +264,9 @@
 #define TWI0_INSTANCE_INDEX      0
 #endif
 
-#define TWI1_ENABLED 0
+#ifndef TWI1_ENABLED
+  #define TWI1_ENABLED 0
+#endif
 
 #if (TWI1_ENABLED == 1)
 #define TWI1_CONFIG_FREQUENCY    NRF_TWI_FREQ_100K
@@ -244,7 +280,9 @@
 #define TWI_COUNT                (TWI0_ENABLED+TWI1_ENABLED)
 
 /* TWIS */
-#define TWIS0_ENABLED 0
+#ifndef TWIS0_ENABLED
+  #define TWIS0_ENABLED 0
+#endif
 
 #if (TWIS0_ENABLED == 1)
     #define TWIS0_CONFIG_ADDR0        0
@@ -256,7 +294,9 @@
     #define TWIS0_INSTANCE_INDEX      0
 #endif
 
-#define TWIS1_ENABLED 0
+#ifndef TWIS1_ENABLED
+  #define TWIS1_ENABLED 0
+#endif
 
 #if (TWIS1_ENABLED ==  1)
     #define TWIS1_CONFIG_ADDR0        0
@@ -287,7 +327,9 @@
 
 
 /* QDEC */
-#define QDEC_ENABLED 0
+#ifndef QDEC_ENABLED
+  #define QDEC_ENABLED 0
+#endif
 
 #if (QDEC_ENABLED == 1)
 #define QDEC_CONFIG_REPORTPER    NRF_QDEC_REPORTPER_10
@@ -303,7 +345,9 @@
 #endif
 
 /* SAADC */
-#define SAADC_ENABLED 0
+#ifndef SAADC_ENABLED
+  #define SAADC_ENABLED 0
+#endif
 
 #if (SAADC_ENABLED == 1)
 #define SAADC_CONFIG_RESOLUTION      NRF_SAADC_RESOLUTION_10BIT
@@ -312,7 +356,9 @@
 #endif
 
 /* LPCOMP */
-#define LPCOMP_ENABLED 0
+#ifndef LPCOMP_ENABLED
+  #define LPCOMP_ENABLED 0
+#endif
 
 #if (LPCOMP_ENABLED == 1)
 #define LPCOMP_CONFIG_REFERENCE    NRF_LPCOMP_REF_SUPPLY_4_8
@@ -322,7 +368,9 @@
 #endif
 
 /* WDT */
-#define WDT_ENABLED 0
+#ifndef WDT_ENABLED
+  #define WDT_ENABLED 0
+#endif
 
 #if (WDT_ENABLED == 1)
 #define WDT_CONFIG_BEHAVIOUR     NRF_WDT_BEHAVIOUR_RUN_SLEEP
